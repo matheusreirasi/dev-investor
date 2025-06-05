68443e18aab49951f7b53a228dc3234674db49cd
Matheus Felipe <reirasi.service@gmail.com>
2025-06-05 11:17:56 -0300

    feature: configuracao dos CORS usada nas rotas

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,17 @@
-from route import app
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from route import router
+from routes import acoes, fiis
 
-app
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+app.include_router(acoes.router, prefix='/api')
+app.include_router(fiis.router, prefix='/api')
