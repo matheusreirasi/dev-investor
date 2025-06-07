8d224b78b83726ca1386bf4f57ee1deee6cebd16
Matheus Felipe <reirasi.service@gmail.com>
2025-06-07 11:23:31 -0300

    default: inseri uma descricao da existencia da aplicacao

--- frontend/app.py
+++ frontend/app.py
@@ -2,17 +2,13 @@ import streamlit as st
 import pandas as pd
 import requests
 
-st.set_page_config(page_title="Fundamentus Dashboard", layout="wide")
+from helpers.data import get_data
+
+st.set_page_config(page_title="DevInvestor", layout="wide")
 st.title("📊 Fundamentus - Ações e FIIs")
 
-@st.cache_data(ttl=3600)
-def get_data(endpoint):
-    url = f"http://backend:8000/{endpoint}"
-    return pd.DataFrame(requests.get(url).json())
-
-st.subheader("📈 AÇÕES")
-st.dataframe(get_data('acoes'), use_container_width=True)
-
-st.subheader("🏢 FIIs")
-st.dataframe(get_data('fiis'), use_container_width=True)
-
+st.write("""
+        Plataforma criada com o intuito de retornar as ações e os fundos imobiliários
+        mais indicados de acordo com as análises fundamentalistas mais utilizadas pelo mercado,
+        auxiliando no processo de escolha de ativos para minha carteira de investimentos.
+        """)
